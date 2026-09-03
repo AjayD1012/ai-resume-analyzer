a1ae7f22ef166d526e2fddd8252ba66e323ed42e
AjayD1012 <ajaydadke10@gmail.com>
2026-09-03 16:09:29 +0530

    feat: initial Spring Boot setup with health endpoint

---
+++ src/test/java/com/airesume/ai_analyzer/AiAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airesume.ai_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
